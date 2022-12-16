4a8764bfbd721e64fda0e128f16fb45dbfb04687
João Matheus Batista de Oliveira <joao.matheeus1555@gmail.com>
2022-12-16 19:07:55 -0300

    feet:Adicionado contador de notificações

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,21 @@
 const markAllAsRead = document.querySelectorAll(".item-notification");
-
+let amoutUnread = document.querySelector("#count_notifications");
+let cont = markAllAsRead.length;
+let containsClass = 0;
 markAllAsRead.forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.remove("read");
   });
+  if (item.classList.contains("read")) {
+    containsClass++;
+  }
 });
+amoutUnread.innerHTML = containsClass;
 
 function clearNotifications() {
   markAllAsRead.forEach((item) => {
     item.classList.remove("read");
+    containsClass--;
+    console.log(containsClass);
   });
 }
